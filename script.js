5c384790f0e80dd5c95d72fbbe6a508cd75f1fa2
Edu Canales <131183212+educanales@users.noreply.github.com>
2025-09-24 12:31:09 -0300

    Add getData and processData functions

---
+++ script.js
@@ -0,0 +1,33 @@
+// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=${API_KEY}
+
+async function getData(location) {
+  const API_KEY = "BP242W9Z9DZAZ4TKRAWX6CTA7";
+
+  try {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=metric&key=${API_KEY}&contentType=json`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function processData(location) {
+  const data = await getData(location);
+  const weatherData = [{
+    address : data.address,
+    temp : data.currentConditions.temp,
+    description : data.description,
+  }]
+  console.log(weatherData);
+  console.log(weatherData[0].address);
+  console.log(weatherData[0].temp);
+  console.log(weatherData[0].description);
+}
+
+function displayData() {
+  
+}
+
+// getData("Dublin");
+processData("New York");
